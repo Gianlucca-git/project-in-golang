96a5e33e1ca1d5dde5ccc074e5e14adf6b03ff10
gaguado <gaguado@payvalida.com>
2022-12-05 05:45:06 -0500

    [Endpoint-3] Creación DB
    Cambios en este commit:
     - Modelo entidad relación
     - creación de tablas
     - inserción de registros
     - data_base_init.sql para iniciar la BD
    
    ¿Para qué se hace este cambio?
     - preparar DB para desarrollo de enpoints item 3

--- data_base_init.sql
+++ data_base_init.sql
@@ -1,3 +1,76 @@
-GRANT
-ALL
-ON DATABASE "gian-project-db" TO postgres;
+drop table if exists public.identification_type cascade;
+create table if not exists public.identification_type (
+                                     id serial primary key ,
+                                     abbreviation varchar(5) unique not null ,
+                                     name varchar unique not null
+);
+INSERT INTO public.identification_type (id, abbreviation, name) VALUES (1, 'CC', 'Cedula de Ciudadania');
+INSERT INTO public.identification_type (id, abbreviation, name) VALUES (2, 'TI', 'Tarjeta de Identidad');
+INSERT INTO public.identification_type (id, abbreviation, name) VALUES (3, 'CE', 'Cedula de Extranjeria');
+INSERT INTO public.identification_type (id, abbreviation, name) VALUES (4, 'P', 'Pasaporte');
+------------------------------------------------------------------------------------------------------------
+drop table if exists public.department cascade;
+create table if not exists public.department (
+                            id serial primary key ,
+                            abbreviation varchar(5) unique not null ,
+                            name varchar unique not null
+);
+INSERT INTO public.department (id, abbreviation, name) VALUES (1, 'ADM', 'Administracion');
+INSERT INTO public.department (id, abbreviation, name) VALUES (2, 'FIN', 'Financiera');
+INSERT INTO public.department (id, abbreviation, name) VALUES (3, 'C', 'COMPRAS');
+INSERT INTO public.department (id, abbreviation, name) VALUES (4, 'INF', 'Infraestructura');
+INSERT INTO public.department (id, abbreviation, name) VALUES (5, 'OP', 'Operacion');
+INSERT INTO public.department (id, abbreviation, name) VALUES (6, 'TH', 'Talento Humano');
+INSERT INTO public.department (id, abbreviation, name) VALUES (7, 'SV', 'Servicios Varios');
+------------------------------------------------------------------------------------------------------------
+drop table if exists public.countries cascade;
+create table if not exists public.countries
+(
+    id serial primary key,
+    abbreviation varchar(5) unique not null ,
+    name varchar unique not null,
+    domain varchar unique not null
+);
+INSERT INTO public.countries (id, abbreviation, name, domain) VALUES (1, 'COL', 'Colombia', '@cidenet.com.co');
+INSERT INTO public.countries (id, abbreviation, name, domain) VALUES (2, 'USA', 'Estados Unidos', '@cidenet.com.us');
+------------------------------------------------------------------------------------------------------------
+DROP TYPE IF EXISTS public.status_user cascade ;
+create type public.status_user as enum ('enable', 'disable', 'stand-by');
+------------------------------------------------------------------------------------------------------------
+drop table if exists public.users cascade;
+create table if not exists public.users
+(
+    id                     uuid         not null
+        primary key,
+    name             varchar(20)  not null,
+    others_names           varchar(20),
+    last_name        varchar(20)  not null,
+    second_last_name       varchar(20)  not null,
+    countries_id           integer
+        constraint users_countries_fk
+            references countries
+            on update cascade,
+    identification_type_id integer
+        constraint users_identification_fk
+            references identification_type
+            on update cascade,
+    identification_number  varchar(20)  not null,
+    mail                   varchar(300) not null
+        unique,
+    admission              date    not null CHECK ( (admission <= current_timestamp)),
+    registration           timestamp    not null CHECK (registration >= admission),
+    department_id          integer
+        constraint users_department_fk
+            references department
+            on update cascade on delete cascade,
+    status                 status_user default 'enable'
+);
+INSERT INTO public.users(id, name, others_names, last_name, second_last_name, countries_id, identification_type_id, identification_number, mail, admission, registration, department_id, status) VALUES ('2314cdbe-c6c2-4a16-a70b-92c149452eb1', 'Laura', 'Daniela', 'Aguado', 'Rendon', 1, 1, '00010', 'laura.daniela0@correo.com.co', '2022-01-03', '2022-01-12 18:08:05.000000', 4, 'enable');
+INSERT INTO public.users (id, name, others_names, last_name, second_last_name, countries_id, identification_type_id, identification_number, mail, admission, registration, department_id, status) VALUES ('a28bb51d-961a-4a09-97b8-90a847627afe', 'Andres', 'Lucca', 'Kennedy', 'Rendon', 1, 1, '1116238356', 'andres.kennedy@correo.com.co', '2022-01-03', '2022-01-12 18:08:05.000000', 4, 'enable');
+INSERT INTO public.users (id, name, others_names, last_name, second_last_name, countries_id, identification_type_id, identification_number, mail, admission, registration, department_id, status) VALUES ('0603848a-e623-4e60-b961-383fcb039cb1', 'Aura', '', 'Cadena', 'Rendon', 1, 1, '1116238356', 'aura.cadena@correo.com.co', '2022-01-03', '2022-01-12 18:08:05.000000', 4, 'enable');
+INSERT INTO public.users (id, name, others_names, last_name, second_last_name, countries_id, identification_type_id, identification_number, mail, admission, registration, department_id, status) VALUES ('4f8eec07-7bd4-4072-8f19-47b35b9be438', 'Luis', 'Lucca', 'Ocampo', 'Rendon', 2, 1, '1116238356', 'luis.ocampo@correo.com.us', '2022-01-03', '2022-01-12 18:08:05.000000', 4, 'enable');
+INSERT INTO public.users (id, name, others_names, last_name, second_last_name, countries_id, identification_type_id, identification_number, mail, admission, registration, department_id, status) VALUES ('68d9e5ac-a147-41e7-a94f-2562cfb951df', 'Carlos', 'Lucca', 'Mendoza', 'Rendon', 1, 1, '1116238356', 'carlos.mendoza@correo.com.co', '2022-01-03', '2022-01-12 18:08:05.000000', 4, 'enable');
+INSERT INTO public.users (id, name, others_names, last_name, second_last_name, countries_id, identification_type_id, identification_number, mail, admission, registration, department_id, status) VALUES ('90b37096-2518-4ade-bdc3-f25393e65124', 'Gian', '', 'Lopez', 'Rendon', 2, 1, '1116238356', 'gian.lopez@correo.com.us', '2022-01-03', '2022-01-12 18:08:05.000000', 4, 'enable');
+INSERT INTO public.users(id, name, others_names, last_name, second_last_name, countries_id, identification_type_id, identification_number, mail, admission, registration, department_id, status) VALUES ('03fdc57f-e9bf-47d0-a25a-255f4a7f894b', 'Gian', 'Lucca', 'Aguado', 'Rendon', 2, 1, '1116238356', 'gian.aguado@correo.com.us', '2022-01-03', '2022-01-12 18:08:05.000000', 4, 'enable');
+INSERT INTO public.users (id, name, others_names, last_name, second_last_name, countries_id, identification_type_id, identification_number, mail, admission, registration, department_id, status) VALUES ('60e1ec05-49c6-4944-807e-afc96d26c4bf', 'Nancy', 'Lucca', 'Parra', 'Ocampo', 1, 1, '1001', 'nancy.parra@correo.com.co', '2022-01-03', '2022-01-12 18:08:05.000000', 4, 'enable');
+INSERT INTO public.users (id, name, others_names, last_name, second_last_name, countries_id, identification_type_id, identification_number, mail, admission, registration, department_id, status) VALUES ('c822c1e2-247d-4bb2-83fd-daf82773afd2', 'alberto', 'alfoso', 'de la calle', 'Ruiz', 1, 1, '01', 'alberto.delacalle@correo.com.co', '2022-01-03', '2022-01-12 13:00:00.000000', 4, 'enable');
